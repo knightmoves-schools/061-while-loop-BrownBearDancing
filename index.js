dcc2b3d9b82fb86baa9ee3febbd4b0accb1bb649
BrownBearDancing <143032898+BrownBearDancing@users.noreply.github.com>
2025-12-02 15:06:17 -0600

    notes

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const updated = markAsDone(tasks);
 console.log(updated);
 
 //should create a function named markAsDone that takes an array of todos and returns the modified array
-/should loop through the todos, using a while loop, and prepend the string 'done - ' to each todo description
+//should loop through the todos, using a while loop, and prepend the string 'done - ' to each todo description
+
 
 
