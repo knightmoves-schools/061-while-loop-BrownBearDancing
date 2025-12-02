7007c60195e81ba1c6c9736d36da2f29ee566988
BrownBearDancing <143032898+BrownBearDancing@users.noreply.github.com>
2025-12-02 14:33:25 -0600

    add tasks

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ function markAsDone(todos) {
   }
   return todos;
 }
+const tasks = ["wash dishes", "clean room", "switch laundry"];
+const updated = markAsDone(tasks);
 
+console.log(updated);
 
 //should create a function named markAsDone that takes an array of todos and returns the modified array
 /should loop through the todos, using a while loop, and prepend the string 'done - ' to each todo description
+
 
