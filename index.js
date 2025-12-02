ff03b2879fced1df7527829d0afd17a9d4e8d276
BrownBearDancing <143032898+BrownBearDancing@users.noreply.github.com>
2025-12-02 14:30:58 -0600

    removed spaces

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function markAsDone(todos) {
   let i = 0;
-  while (i < todos.length) {
-    todos [i] = 'done - ' + todos [i];
+  while(i < todos.length) {
+    todos[i] = "done - " + todos [i];
     i++;
   }
   return todos;
@@ -10,3 +10,4 @@ function markAsDone(todos) {
 
 //should create a function named markAsDone that takes an array of todos and returns the modified array
 /should loop through the todos, using a while loop, and prepend the string 'done - ' to each todo description
+
