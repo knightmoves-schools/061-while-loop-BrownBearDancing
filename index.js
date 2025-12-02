b5f528209ccd7acb1b5303d383d49deeaa09eeaf
BrownBearDancing <143032898+BrownBearDancing@users.noreply.github.com>
2025-12-02 14:27:13 -0600

    check if correct

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+function markAsDone(todos) {
+  let i = 0;
+  while (i < todos.length) {
+    todos [i] = 'done - ' + todos [i];
+    i++;
+  }
+  return todos;
+}
 
+
+//should create a function named markAsDone that takes an array of todos and returns the modified array
+/should loop through the todos, using a while loop, and prepend the string 'done - ' to each todo description
